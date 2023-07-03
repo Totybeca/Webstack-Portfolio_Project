7e2a02d9fb3893585f177237566201235a12720c
oishbdh <ikechukwuarinze614@gmail.com>
2023-07-03 05:35:40 +0100

    Webstack

--- Electronic/ElectronicWebAPI/urls.py
+++ Electronic/ElectronicWebAPI/urls.py
@@ -1,23 +1,13 @@
 from django.urls import path
-from django.views.decorators.cache import never_cache
-from debug_toolbar.views import render_panel
-import debug_toolbar
-from . import views
-from .views import CustomUserAPIView
-from .views import LoginFormView
-from django.contrib.auth.views import LoginView
 
-app_name = 'ElectronicWebAPI'
 
 urlpatterns = [
-    path('render_panel/', never_cache(render_panel), name='render_panel'),
-    path('users/', CustomUserAPIView.as_view(), name='custom_users'),
-    path('register/', views.register, name='register'),
-    path('base/', views.base, name='base'),
-    path('home/', views.home, name='home'),
-    path('about/', views.about, name='about_us'),
-    path('contact/', views.contact, name='contact_us'),
-    path('product/', views.product, name='our_product'),
-    path('categories/', views.categories, name='all_categories'),
-   path('login/', LoginView.as_view(template_name='login.html'), name='login'),
+    path('register', views.register, name="register"),
+    path('base', views.base, name="base"),
+    path('home', views.home, name="home"),
+    path('about', views.about, name="about_us"),
+    path('contact', views.contact, name="contact_us"),
+    path('product', views.product, name="our_product"),
+    path('categories', views.categories, name="all_categories"),
+   path('login', views.login, name="login")
 ]
--- Electronic/ElectronicWebAPI/views.py
+++ Electronic/ElectronicWebAPI/views.py
@@ -1,46 +1,64 @@
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import user_passes_test
 from django.shortcuts import render, redirect
-from .forms import SignUpForm
-from rest_framework import status
-from rest_framework.response import Response
-from .serializers import CustomUserSerializer
-from django.views import View
-from .models import CustomUser
-from django.contrib.auth.views import LoginView
-from django.urls import reverse_lazy
-from django.contrib.auth.forms import AuthenticationForm
+from django.conf import settings
+from django.utils import timezone
+from random import randint
+from django.contrib import messages
+from django.contrib.auth import SESSION_KEY, authenticate, login
+from django.contrib.auth.models import user, auth
 
 # Create views here
 
-class CustomUserAPIView(View):
-    def get(self, request):
-        custom_users = CustomUser.objects.all()
-        serializer = CustomUserSerializer(custom_users, many=True)
-        return Response(serializer.data)
+def anonymous_required(function=None, redirect_url=None):
+    
+   if not redirect_url:
+       redirect_url = 'dashboard'
 
-    def post(self, request):
-        serializer = CustomUserSerializer(data=request.POST)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+   actual_decorator = user_passes_test(
+       lambda u: u.is_anonymous,
+       login_url=redirect_url
+   )
+
+   if function:
+       return actual_decorator(function)
+   return actual_decorator
 
 
+
+
+#using the long-required decorator
+@login_required
+def logout(request):
+  auth.logout(request)
+  return redirect('login')
+
+
+
+
+#using the anonymous required decorator
+@anonymous_required
+def login(request):
+    context = {"Message": 'Successful'}
+    return render(request, 'login', context)
+    
+    
+    
+  
+@anonymous_required
 def register(request):
-    if request.method == 'POST':
-        form = SignUpForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            # Display a success message
-            messages.success(request, 'Registration successful. Please log in.')
-            return redirect('login')  # Redirect to the login page
-    else:
-        form = SignUpForm()
-    return render(request, 'register.html', {'form': form})
+    context = {"Message": 'Successful'}
+    return render(request, 'login', context)
 
-class LoginFormView(LoginView):
-    template_name = 'login.html'  # Path to your login template
-    form_class = AuthenticationForm
-    success_url = reverse_lazy('home')  # URL to redirect after successful login
+@login_required
+def dashboard(request):
+    context = {"Message": 'Successful'}
+    return render(request, 'dashboard', context)
+
+
+
+
+
 
 
 def base(request):
